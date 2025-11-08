da27fae651d0c581b76296829ad9cd00d255af23
brandon <isbrandon@pm.me>
2025-11-08 14:57:37 -0800

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,69 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  extends: ["shadcn-docs-nuxt"],
+
+  i18n: {
+    defaultLocale: "en",
+    locales: [
+      {
+        code: "en",
+        name: "English",
+        language: "en-US",
+      },
+    ],
+  },
+
+  compatibilityDate: "2024-07-06",
+
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "@vueuse/nuxt",
+    "@nuxt/icon",
+    "@nuxt/fonts",
+    "@samk-dev/nuxt-vcalendar",
+    "@vee-validate/nuxt",
+    "@morev/vue-transitions/nuxt"
+  ],
+
+  tailwindcss: {
+    exposeConfig: true,
+    editorSupport: true,
+  },
+
+  colorMode: {
+    classSuffix: "",
+  },
+
+  imports: {
+    imports: [{
+      from: "tailwind-variants",
+      name: "tv",
+    }, {
+      from: "tailwind-variants",
+      name: "VariantProps",
+      type: true,
+    }, {
+      from: "vue-sonner",
+      name: "toast",
+      as: "useSonner"
+    }],
+  },
+
+  app: {
+    head: {
+      script: [{
+        src: "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.12/pdfmake.min.js",
+        defer: true
+      }, {
+        src: "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.12/vfs_fonts.min.js",
+        defer: true
+      }]
+    }
+  },
+
+  build: {
+    transpile: ["vue-sonner"]
+  }
+});
